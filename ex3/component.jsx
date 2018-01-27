3455612ba805b1bc83e3d7c52f39abc0ceed55d7
filipedesousab <filipegame07@gmail.com>
2018-01-26 22:31:11 -0300

    Primeiro Commit

---
+++ ex3/component.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Primeiro = (props) => {
+  return <h1>Primeiro Component</h1>
+}
+
+export const Segundo = (props) => <h1>Segundo Component</h1>
+
+//export { Primeiro, Segundo }
---
+++ ex6/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Family from './family'
+import Member from './member'
+
+ReactDOM.render(
+  <div>
+    <Family lastName='Silva'>
+      <Member name='Guilherme' />
+      <Member name='Julia' />
+      <Member name='Maria' />
+    </Family>
+  </div>
+, document.getElementById('app'))
